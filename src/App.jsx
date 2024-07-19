2571ee4f7e12afac4b78d8714e9829e1fb8ccfaf
racp7667 <potlurisc615@gmail.com>
2024-07-19 10:56:49 -0500

    added code for hero section

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import Hero from './sections/Hero/Hero';
+
+function App() {
+  return<><Hero></Hero></>
+}
+
+export default App;
---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/hero-img.png";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedInLight from "../../assets/linkedin-light.svg";
+import linkedInDark from "../../assets/linkedin-dark.svg";
+import resume from "../../assets/saiPotluriSDEResume.pdf";
+import { useTheme } from "../../common/ThemeContext";
+
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+  const themeIcon = theme === "light" ? sun : moon;
+  const linkedInIcon = theme === "light" ? linkedInLight : linkedInDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  return (
+    <section id="hero" className={styles.container}>
+      {/* Hero Section Starts */}
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.hero}
+          src={heroImg}
+          alt="Profile picture of Sai Potluri"
+        />
+        <img
+          src={themeIcon}
+          alt="Color mode icon"
+          className={styles.colorMode}
+          onClick={toggleTheme}
+        />
+      </div>
+      {/* Hero Section Ends */}
+
+      {/* Info Section Starts */}
+      <div className={styles.info}>
+        <h1>
+          Sai
+          <br />
+          Potluri
+        </h1>
+        <h2>Fullstack Developer</h2>
+        <span>
+          <a href="https://linkedin.com" target="_blank">
+            <img src={linkedInIcon} alt="LinkedIn Icon" />
+          </a>
+          <a href="https://github.com/saicherishpotluri" target="_blank">
+            <img src={githubIcon} alt="GitHub Icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+          With a passion for developing modern and interactive web applications.
+        </p>
+        <a href={resume} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
